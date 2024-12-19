664ea35307a90da139cff5966c7727ec5cca2c1c
Lachie <lachlan.philipson@me.com>
2024-12-19 11:29:06 +0100

    Initial Commit

---
+++ RecipeasyTests/RecipeasyTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeasyTests.swift
+//  RecipeasyTests
+//
+//  Created by Lachlan Philipson on 19/12/2024.
+//
+
+import Testing
+
+struct RecipeasyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
